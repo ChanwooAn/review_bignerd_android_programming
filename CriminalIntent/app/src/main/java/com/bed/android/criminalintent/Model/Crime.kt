e83d6dda6f14889f0bbbced44160a3c0e02862b7
acw <acwoo97@gmail.com>
2022-08-23 01:13:25 +0900

    8/23 1:13

--- CriminalIntent/app/src/main/java/com/bed/android/criminalintent/Model/Crime.kt
+++ CriminalIntent/app/src/main/java/com/bed/android/criminalintent/Model/Crime.kt
@@ -1,10 +1,16 @@
 package com.bed.android.criminalintent.Model
 
+import android.annotation.SuppressLint
+import java.text.SimpleDateFormat
 import java.util.*
+
+
+@SuppressLint("SimpleDateFormat")
+val dateformat=SimpleDateFormat("EEE, MMM dd yyyy, kk : mm aaa ")
 
 data class Crime(
     val id:UUID=UUID.randomUUID(),
     var title:String="",
-    var date:Date =Date(),
+    var date: String = dateformat.format(Date()),
     var isSolved:Boolean = false
 )
--- CriminalIntent/app/src/main/java/com/bed/android/criminalintent/adapter/CrimeAdapter.kt
+++ CriminalIntent/app/src/main/java/com/bed/android/criminalintent/adapter/CrimeAdapter.kt
@@ -1,23 +1,32 @@
 package com.bed.android.criminalintent.adapter
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bed.android.criminalintent.Model.Crime
 import com.bed.android.criminalintent.R
-
+private const val TAG="CrimeAdapter"
 class CrimeAdapter(var crimes:List<Crime>,var context:Context) : RecyclerView.Adapter<CrimeAdapter.CrimeHolder>(){
 
     inner class CrimeHolder(view: View) :RecyclerView.ViewHolder(view){
-        val titleTextView:TextView=view.findViewById(R.id.crime_title)
-        val dateTextView:TextView=view.findViewById(R.id.crime_date)
+        private val titleTextView:TextView=view.findViewById(R.id.crime_title)
+        private val dateTextView:TextView=view.findViewById(R.id.crime_date)
+        private val solvedImageView:ImageView=view.findViewById(R.id.crime_solved)
 
         fun bind(crime: Crime){
             titleTextView.text=crime.title
-            dateTextView.text=crime.date.toString()
+            dateTextView.text=crime.date
+            Log.d(TAG,crime.date)
+            solvedImageView.visibility=if(crime.isSolved){
+                View.VISIBLE
+            }else{
+                View.INVISIBLE
+            }
         }
     }
 
